e3e6a3758f8f7c5ba3008e7d90aa5a221d3e556f
simachami <simabchami@gmail.com>
2023-07-27 13:00:18 -0600

    secret key

--- Day4/Inclass/js/script.js
+++ Day4/Inclass/js/script.js
@@ -1,50 +1,62 @@
 console.log('Matrix Spotify Project')
 
-const clientId = '47db93762e094997a07ac0f2706ffef2'
-const clientSecret = 'b7f45f4963aa486d8a857aaad66984e0'
+
+let audio
 
 
-async function getToken() {
-    const response = await fetch('https://accounts.spotify.com/api/token', {
-        method: 'POST', 
-        body: 'grant_type=client_credentials',
-        headers: {
-            Authorization: `Basic ${btoa(clientId + ':' + clientSecret)}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    })
-    if(response.ok){
-        const data = await response.json()
-        return data.access_token
-    } else window.alert ('Bad request')
+async function getToken(){
+  const response = await fetch('https://accounts.spotify.com/api/token',{
+    method : 'POST',
+    body : 'grant_type=client_credentials',
+    headers : {
+      Authorization: `Basic ${btoa(clientId + ':' + clientSecret)}`,
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+  if(response.ok){
+    const data = await response.json()
+    return data.access_token
+  } else window.alert('Bad request')
 }
 
-(async ()=> await getToken())()
+// (async ()=> console.log(await getToken()))()
 
-async function getSong(track, artist) {
-    const res = await fetch(`https://api.spotify.com/v1/search?q=${track},${artist}&type=track,artist&limit=5`,{
-        method: 'GET',
-        headers : {
-            Authorization: `Bearer ${await getToken()}`,
-            'Content-Type': 'application/json'
-        }
-    })
-    if(res.ok){
-        const data = await res.json()
-        console.log(data.tracks.items[0].preview_url)
-    } else window.alert('Bad Request')
+async function getSong(track, artist){
+  const res = await fetch(`https://api.spotify.com/v1/search?q=${track},${artist}&type=track,artist&limit=5`,{
+    method : 'GET',
+    headers : {
+      Authorization : `Bearer ${await getToken()}`,
+      'Content-Type' : 'application/json'
+    }
+  })
+  if(res.ok){
+    const data = await res.json()
+    return data.tracks.items[0].preview_url
+  } else window.alert('Bad Request')
 }
 
-
-(async ()=> await getSong('fame', 'david bowie'))()
-
+// (async ()=> await getSong('fame','david bowie'))()
 
 const imgs = document.getElementsByTagName('img')
-console.log(imgs)
-for(const img of imgs){
-    img.addEventListener('click', {})
+for( const img of imgs ){
+  img.addEventListener('click', async () => {
+    const [song, artist] = img.alt.split(' - ')
+    const url = await getSong(song, artist)
+    if (audio) stopSong()
+    playSong(url)
+  })
 }
 
-function playSong() {}
+function playSong(url){
+  audio = new Audio(url)
+  audio.volume = .5
+  audio.play()
+}
 
+function stopSong(){
+    audio.pause()
+}
+
+
+document.getElementById('stop-btn').addEventListener('click',stopSong)
 
---
+++ Day4/Inclass/js/secrets.js
@@ -0,0 +1,2 @@
+const clientId = 'b31ebfacc1304dec8e4cdeed32ceba82'
+const clientSecret = 'f635430f58f24a2994df3c4fff574636'
